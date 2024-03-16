f40f685610505205d43be00c389e92e7c93078c1
vando09 <145195909+vando09@users.noreply.github.com>
2024-03-16 18:36:42 +0700

    ChinhTiengViet-Footer

--- client/particals/footer.php
+++ client/particals/footer.php
@@ -10,14 +10,14 @@
 				<div class="subscription-form">
 					<h3 class="d-flex align-items-center"><span class="me-1"><img
 								src="/client/assets/images/envelope-outline.svg" alt="Image"
-								class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
+								class="img-fluid"></span><span>Đăng kí nhận bản tin</span></h3>
 
 					<form action="#" class="row g-3">
 						<div class="col-auto">
-							<input type="text" class="form-control" placeholder="Enter your name">
+							<input type="text" class="form-control" placeholder="Nhập tên của bạn">
 						</div>
 						<div class="col-auto">
-							<input type="email" class="form-control" placeholder="Enter your email">
+							<input type="email" class="form-control" placeholder="Nhập email của bạn">
 						</div>
 						<div class="col-auto">
 							<button class="btn btn-primary">
@@ -33,9 +33,7 @@
 		<div class="row g-5 mb-5">
 			<div class="col-lg-4">
 				<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-				<p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus
-					malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
-					Pellentesque habitant</p>
+				<p class="mb-4">Đậm đà hương vị cà phê</p>
 
 				<ul class="list-unstyled custom-social">
 					<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
@@ -49,35 +47,35 @@
 				<div class="row links-wrap">
 					<div class="col-6 col-sm-6 col-md-3">
 						<ul class="list-unstyled">
-							<li><a href="#">About us</a></li>
-							<li><a href="#">Services</a></li>
-							<li><a href="#">Blog</a></li>
-							<li><a href="#">Contact us</a></li>
+							<li><a href="#">Về chúng tôi</a></li>
+							<li><a href="#">Dịch vụ</a></li>
+							<li><a href="#">Tin tức</a></li>
+							<li><a href="#">Liên hệ</a></li>
 						</ul>
 					</div>
 
 					<div class="col-6 col-sm-6 col-md-3">
 						<ul class="list-unstyled">
-							<li><a href="#">Support</a></li>
-							<li><a href="#">Knowledge base</a></li>
-							<li><a href="#">Live chat</a></li>
+							<li><a href="#">Hỗ trợ</a></li>
+							<li><a href="#">Cơ cấu</a></li>
+							<li><a href="#">Trò chuyện trực tiếp</a></li>
 						</ul>
 					</div>
 
 					<div class="col-6 col-sm-6 col-md-3">
 						<ul class="list-unstyled">
-							<li><a href="#">Jobs</a></li>
-							<li><a href="#">Our team</a></li>
-							<li><a href="#">Leadership</a></li>
-							<li><a href="#">Privacy Policy</a></li>
+							<li><a href="#">Công việc</a></li>
+							<li><a href="#">Đội ngũ của chúng tôi</a></li>
+							<li><a href="#">Lãnh đạo</a></li>
+							<li><a href="#">Chính sánh bảo mật</a></li>
 						</ul>
 					</div>
 
 					<div class="col-6 col-sm-6 col-md-3">
 						<ul class="list-unstyled">
-							<li><a href="#">Nordic Chair</a></li>
-							<li><a href="#">Kruzo Aero</a></li>
-							<li><a href="#">Ergonomic Chair</a></li>
+							<li><a href="#">Chính sách đổi trả</a></li>
+							<li><a href="#">Giao hàng miễn phí</a></li>
+							<li><a href="#">Phục vụ 24/7</a></li>
 						</ul>
 					</div>
 				</div>
