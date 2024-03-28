a4f49d61d3e96a39f7dad31477feff162859b234
vando09 <145195909+vando09@users.noreply.github.com>
2024-03-28 19:54:07 +0700

    list-statistic

---
+++ admin/modules/statistic/list.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+
+    <title>Thống kê</title>
+</head>
+
+<body>
+    <div id="myfirstchart" style="height: 250px;"></div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    <script>
+        new Morris.Line({
+            // ID of the element in which to draw the chart.
+            element: 'myfirstchart',
+            // Chart data records -- each entry in this array corresponds to a point on
+            // the chart.
+            data: [{
+                    year: '2020',
+                    value: 20
+                }, {
+                    year: '2021',
+                    value: 20
+                },
+                {
+                    year: '2022',
+                    value: 10
+                },
+                {
+                    year: '2023',
+                    value: 5
+                },
+                {
+                    year: '2024',
+                    value: 5
+                }
+
+            ],
+            // The name of the data record attribute that contains x-values.
+            xkey: 'year',
+            // A list of names of data record attributes that contain y-values.
+            ykeys: ['value'],
+            // Labels for the ykeys -- will be displayed when you hover over the
+            // chart.
+            labels: ['Value']
+        });
+    </script>
+</body>
+
+</html>
