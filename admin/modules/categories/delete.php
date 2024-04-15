0bb09c47a42ff8590d01fa2a811e3a1d8b3e6f5d
vando09 <145195909+vando09@users.noreply.github.com>
2024-04-15 11:37:32 +0700

    sửa lại giao diện và bắt lỗi form

--- admin/modules/categories/delete.php
+++ admin/modules/categories/delete.php
@@ -46,6 +46,44 @@ if (isset($_POST['confirm_delete'])) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+} else {
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+      <h2>Xác nhận xóa danh mục</h2>
+      <form action="" method="post">
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <p>Bạn có chắc chắn muốn xóa danh mục này?</p>
+        <button type="submit" name="confirm_delete" class="btn btn-danger">Xóa</button>
+        <a href="/admin/?act=categories&page=list" class="btn btn-secondary">Hủy</a>
+      </form>
+    </div>
+  </div>
+</div>
+
+<?php
+}
+
+if (isset($_POST['confirm_delete'])) {
+    $sql = "DELETE FROM categories WHERE id = $id";
+    $result = $conn->query($sql);
+
+    if ($result) {
+        $sql = "SELECT thumbnail FROM categories WHERE id = $id";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $thumbnail = $row['thumbnail'];
+        unlink($thumbnail);
+
+        $sql = "DELETE FROM categories_name WHERE category_id = $id";
+        $result = $conn->query($sql);
+
+        header("Location:/admin/?act=categories&page=list");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
 $conn->close();
--- admin/modules/categories/list.php
+++ admin/modules/categories/list.php
@@ -12,7 +12,7 @@
             <tbody>
               <?php
               $select = "SELECT * FROM categories";
-              $result = $connect->query($select);
+              $result = $conn->query($select);
               while ($item = $result->fetch_assoc()) :
               ?>
                 <tr>
@@ -21,21 +21,25 @@
                   <th scope="col"><img src="<?= $item['thumbnail'] ?>" width="100" height="150"></th>
                   <th scope="col">
                     
-                    <a href="/admin/?act=categories&page=update<?= $item['id'] ?>" type="submit" name="update" class="btn btn-primary m-2">Cập nhật</a>
+                    <a href="/admin?act=categories&page=update<?= $item['id'] ?>" type="submit" name="update" class="btn btn-primary m-2">Cập nhật</a>
 
 
-                    <form action="/admin/?act=categories&page=delete&id=<?= $item['id'] ?>" method="POST">
+                    <form action="/admin?act=categories&page=delete&id=<?= $item['id'] ?>" method="POST">
                       <input type="submit" name="delete" class="btn btn-primary m-2" value="Xóa" />
                     </form>
 
-        </th>
-      </tr>
-
-    <?
-    endwhile;
+                <a type="submit" name="edit" href="/admin/?act=categories&page=update&id=<?= $item['id'] ?>" class="btn btn-warning m-2"><i class='fas fa-pencil-alt'></i></a>
 
 
 
-    $conn->close();
-    ?>
+              </td>
+            </tr>
+          <?php
+          endwhile; ?>
+        </tbody>
       </table>
+    </div>
+  </div>
+  <!-- /.card-body -->
+</div>
+<!-- /.card -->
